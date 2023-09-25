ea052ef4a2339937d42749424c98cc108c0e7166
Ryan Olson <ry4nolson@gmail.com>
2023-09-25 15:27:45 -0500

    include cart component

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import React from "react";
 import products from './data'
 import ProductCard from "./components/Product";
 
-
 function App() {
-
   return (
-    <div className="beard-prime">
-    {
-      products.map((product) => <ProductCard product={product} />)
-    }
-    </div>
+    <>
+      <div className="beard-prime">
+      {
+        products.map((product) => <ProductCard product={product} key={product.sku} />)
+      }
+      </div>
+      <div
+        id="amzn-bwp-cart"
+        data-site-id="tds3sary6j"
+        data-widget-id="w-UA7MN8pHGj6YDHJqCGhQD1"></div>
+    </>
   );
 }
 export default App;
